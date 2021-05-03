d7db3ba688462e08169cf55d4acf6777d3d0f3d8
borg <felipebpl@al.insper.edu.br>
2021-05-03 14:51:19 -0300

    regressao linear

--- scripts/projeto.py
+++ scripts/projeto.py
@@ -20,9 +20,13 @@ from geometry_msgs.msg import Twist, Vector3, Pose, Vector3Stamped
 from nav_msgs.msg import Odometry
 from std_msgs.msg import Header
 
-print("EXECUTE ANTES da 1.a vez: ")
-print("wget https://github.com/Insper/robot21.1/raw/main/projeto/ros_projeto/scripts/MobileNetSSD_deploy.caffemodel")
-print("PARA TER OS PESOS DA REDE NEURAL")
+
+from sklearn.linear_model import LinearRegression
+
+#print("EXECUTE ANTES da 1.a vez: ")
+#print("wget https://github.com/Insper/robot21.1/raw/main/projeto/ros_projeto/scripts/MobileNetSSD_deploy.caffemodel")
+#print("PARA TER OS PESOS DA REDE NEURAL")
+
 
 import visao_module
 
@@ -54,6 +58,152 @@ frame = "camera_link"
 tfl = 0
 
 tf_buffer = tf2_ros.Buffer()
+
+def encontrar_centro_dos_contornos(img, contornos):
+    """Não mude ou renomeie esta função
+        deve receber um contorno e retornar, respectivamente, a imagem com uma cruz no centro de cada segmento e o centro dele. formato: img, x, y
+    """
+    centrox = []
+    centroy = []
+
+    for i in contornos:
+        centro_x2, centro_y2 = center_of_mass(i)
+        centrox.append(centro_x2)
+        centroy.append(centro_y2)
+        crosshair(img,(centro_x2, centro_y2), 5, (255,0,0))
+
+    return img, centrox, centroy
+
+def encontrar_contornos(mask):
+    """Não mude ou renomeie esta função
+        deve receber uma imagem preta e branca os contornos encontrados
+    """
+    contornos, arvore = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    return contornos
+
+def crosshair(img, point, size, color):
+    """ Desenha um crosshair centrado no point.
+        point deve ser uma tupla (x,y)
+        color é uma tupla R,G,B uint8
+    """
+    x,y = point
+    cv2.line(img,(x - size,y),(x + size,y),color,2)
+    cv2.line(img,(x,y - size),(x, y + size),color,2)
+
+def center_of_mass(mask):
+    """ Retorna uma tupla (cx, cy) que desenha o centro do contorno"""
+    M = cv2.moments(mask)
+    # Usando a expressão do centróide definida em: https://en.wikipedia.org/wiki/Image_moment
+    
+    m00 = M["m00"]
+
+    if m00 == 0:
+        m00 = 1
+
+    cX = int(M["m10"] / m00)
+    cY = int(M["m01"] / m00)
+    return [int(cX), int(cY)]
+
+def center_of_mass_region(mask, x1, y1, x2, y2):
+    # Para fins de desenho
+    mask_bgr = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+    clipped = mask[y1:y2, x1:x2]
+    c = center_of_mass(clipped)
+    c[0]+=x1
+    c[1]+=y1
+    crosshair(mask_bgr, c, 10, (0,0,255))
+    cv2.rectangle(mask_bgr, (x1, y1), (x2, y2), (255,0,0),2,cv2.LINE_AA)
+    return mask_bgr
+
+
+def desenhar_linha_entre_pontos(img, X, Y, color):
+    """Não mude ou renomeie esta função
+        deve receber uma lista de coordenadas XY, e retornar uma imagem com uma linha entre os centros EM SEQUENCIA do mais proximo.
+    """
+    for i in range(1,len(X)):
+        cv2.line(img,(X[i-1],Y[i-1]),(X[i],Y[i]),color,2)
+    
+    return img
+
+def regressao_por_centro(img, x,y):
+    """Não mude ou renomeie esta função
+        deve receber uma lista de coordenadas XY, e estimar a melhor reta, utilizando o metodo preferir, que passa pelos centros. Retorne a imagem com a reta e os parametros da reta
+        
+        Dica: cv2.line(img,ponto1,ponto2,color,2) desenha uma linha que passe entre os pontos, mesmo que ponto1 e ponto2 não pertençam a imagem.
+    """
+    x_shape = np.array(x)
+    y_shape = np.array(y)
+
+    x_shape = x_shape.reshape(-1,1)
+    y_shape = y_shape.reshape(-1,1)
+
+    regression = LinearRegression()
+
+    regression.fit(x_shape, y_shape)
+
+    w, z = regression.coef_, regression.intercept_
+
+    a_1 = 100
+    a_2 = 10000
+    b_1 = int((a_1*w+z))
+    b_2 = int((a_2*w+z))
+    ponto_a = (a_1,b_1)
+    ponto_b = (a_2,b_2)
+
+    print(ponto_a, ponto_b)
+
+    cor = (255,0,0)
+
+    cv2.line(img,ponto_a,ponto_b,cor,2)
+
+    return img, (w, z)
+
+
+def image_callback(img_cv):
+    # BEGIN BRIDGE
+    #image = bridge.imgmsg_to_cv2(msg)
+    # END BRIDGE
+    # BEGIN HSV
+    hsv = cv2.cvtColor(img_cv, cv2.COLOR_BGR2HSV)
+    # END HSV
+    # BEGIN FILTER
+
+    lower_yellow = numpy.array([25, 50, 50])
+    upper_yellow = numpy.array([35, 255, 255])
+    mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+    
+    kernel = np.ones((5,5),np.uint8)
+
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    # END FILTER
+    masked = cv2.bitwise_and(img_cv, img_cv, mask=mask)
+    cv2.imshow("Filtra Amarelo", mask ) 
+    cv2.waitKey(3)
+
+    #bgr = cv2.cvtColor(HSV, cv2.COLOR_HSV2BGR)\
+    bgr = img_cv.copy()
+
+    contornos = encontrar_contornos(mask)
+    cv2.drawContours(mask, contornos, -1, [0, 0, 255], 2)
+
+    mask_bgr = center_of_mass_region(mask, 20, 400, bgr.shape[1] - 80, bgr.shape[0]-100)
+
+    
+    img, X, Y = encontrar_centro_dos_contornos(mask_bgr, contornos)
+
+    img = desenhar_linha_entre_pontos(mask_bgr, X,Y, (255,0,0))
+
+    
+
+    # Regressão Linear
+    
+    ## Regressão pelo centro
+    img, lm = regressao_por_centro(img, X,Y)
+
+    cv2.imshow("Regressao", img)
+    cv2.waitKey(3)
+
 
 
 
@@ -87,6 +237,7 @@ def roda_todo_frame(imagem):
 
         # Desnecessário - Hough e MobileNet já abrem janelas
         cv_image = saida_net.copy()
+        saida_amarelo = image_callback(cv_image)
         cv2.imshow("cv_image", cv_image)
         cv2.waitKey(1)
     except CvBridgeError as e:
@@ -94,6 +245,7 @@ def roda_todo_frame(imagem):
     
 if __name__=="__main__":
     rospy.init_node("cor")
+
 
     topico_imagem = "/camera/image/compressed"
 
